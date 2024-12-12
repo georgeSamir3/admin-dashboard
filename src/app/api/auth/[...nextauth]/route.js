3fa635ccdc39911dd1ea4dc61394a64f33cb62aa
george samir <georgesamir@georges-MacBook-Air.local>
2024-12-12 23:24:15 +0200

    upload

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,13 +1,21 @@
-// Third-party Imports
-import NextAuth from 'next-auth'
+// // Third-party Imports
+// import NextAuth from 'next-auth'
 
-// Lib Imports
+// // Lib Imports
+// import { authOptions } from '@/libs/auth'
+
+// /*
+//  * As we do not have backend server, the refresh token feature has not been incorporated into the template.
+//  * Please refer https://next-auth.js.org/tutorials/refresh-token-rotation link for a reference.
+//  */
+// const handler = NextAuth(authOptions)
+
+// export { handler as GET, handler as POST }
+
+// In your current [...nextauth].ts file
+import NextAuth, { NextAuthOptions } from 'next-auth'
 import { authOptions } from '@/libs/auth'
 
-/*
- * As we do not have backend server, the refresh token feature has not been incorporated into the template.
- * Please refer https://next-auth.js.org/tutorials/refresh-token-rotation link for a reference.
- */
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
